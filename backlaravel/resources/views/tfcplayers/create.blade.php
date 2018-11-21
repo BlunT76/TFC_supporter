876c6ed7e92aaf263a28231265bd1b4cdc8d97d3
Geoffrey <geoffrey.frioli@gmail.com>
2018-11-21 11:42:20 +0100

    add pattern regex forms html

--- backlaravel/resources/views/tfcplayers/create.blade.php
+++ backlaravel/resources/views/tfcplayers/create.blade.php
@@ -10,23 +10,23 @@
       @csrf
       <div class="form-group">
         <label for="firstname">Prénom</label>
-        <input type="text" class="form-control" id="firstname" name="firstname" placeholder="Entrez le prénom" required>
+        <input type="text" class="form-control" id="firstname" name="firstname" placeholder="Entrez le prénom" required maxlength="50">
       </div>
       <div class="form-group">
         <label for="lastname">Nom</label>
-        <input type="text" class="form-control" id="lastname" name="lastname" placeholder="Entrez le nom" required>
+        <input type="text" class="form-control" id="lastname" name="lastname" placeholder="Entrez le nom" required maxlength="50">
       </div>
       <div class="form-group">
         <label for="number">Numéro</label>
-        <input type="number" class="form-control" id="number" name="number" placeholder="Entrez le numéro du joueur" required>
+        <input type="number" class="form-control" id="number" name="number" placeholder="Entrez le numéro du joueur" required pattern="\d*" min="1" max="99">
       </div>
       <div class="form-group">
         <label for="poste">Poste</label>
-        <input type="text" class="form-control" id="poste" name="position" placeholder="Entrez le poste du joueur" required>
+        <input type="text" class="form-control" id="poste" name="position" placeholder="Entrez le poste du joueur" required maxlength="50">
       </div>
       <div class="form-group">
         <label for="photo">Photo</label>
-        <input type="url" class="form-control" id="photo" name="portrait" placeholder="Entrez l'url de la photo du joueur" required>
+        <input type="url" class="form-control" id="photo" name="portrait" placeholder="Entrez l'url de la photo du joueur" required maxlength="255">
       </div>
       <div class="text-center">
         <button type="submit" class="btn btnTfc" title="Créer un joueur">Envoyer</button>
