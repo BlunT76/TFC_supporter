27fcb990610a7af25cf8f85e704f418f2a363795
Vanessa~Roux <vanessa.roux891@orange.fr>
2018-11-22 11:56:57 +0100

    amélioration style connexion

--- backlaravel/resources/views/auth/login.blade.php
+++ backlaravel/resources/views/auth/login.blade.php
@@ -3,11 +3,13 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Admin Connexion supporter TFC">
+
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>Admin TFC Supporters</title>
+    <title>Admin Connexion TFC Supporters</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
@@ -15,10 +17,13 @@
     <!-- Fonts -->
     <link rel="dns-prefetch" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
+    <link rel="icon" type="image/png" href="images/tfc1.png"/>
+
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link href="{{ asset('css/styles.css') }}" rel="stylesheet">
+
 
 </head>
 <body>
